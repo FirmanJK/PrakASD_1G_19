5e173fd922af634a8ba7ba2b16e2d6f6eb55cd23
FirmanJK <jatikusuma761@gmail.com>
2024-02-13 12:52:50 +0700

    Perkenalan

---
+++ src/P1/LatihanFirman19.java
@@ -0,0 +1,14 @@
+package P1;
+import java.util.Scanner;
+
+/**
+ * LatihanFirman19
+ */
+public class LatihanFirman19 {
+    public static void main (String [] args) {
+       //System.out.println("Nama saya Mochammad Firmandika Jati Kusuma");
+     //System.out.println("saya adalah mahasiswa baru Jurusan Teknologi Informasi-POLINEMA");
+
+    
+    }
+}
