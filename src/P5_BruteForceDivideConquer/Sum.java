051988f178b041c561de9f27102db02e0c4c0493
FirmanJK <jatikusuma761@gmail.com>
2024-03-25 11:15:04 +0700

    P5_Percobaan3 Pembenaran

--- src/P5_BruteForceDivideConquer/Sum.java
+++ src/P5_BruteForceDivideConquer/Sum.java
@@ -24,7 +24,7 @@ public class Sum {
             int mid = (l + r) / 2; 
             double lsum = totalDC(arr, l, mid);
             double rsum = totalDC(arr, mid + 1, r); 
-            return lsum + rsum + mid - 1.5;
+            return lsum + rsum;
         }
         return 0;
     }
