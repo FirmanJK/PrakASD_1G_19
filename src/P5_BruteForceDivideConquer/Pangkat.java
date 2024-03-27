7035777baa70bfb828823f08ebd939a95c87247d
FirmanJK <jatikusuma761@gmail.com>
2024-03-27 10:39:34 +0700

    P5_Pembenaran_19

--- src/P5_BruteForceDivideConquer/Pangkat.java
+++ src/P5_BruteForceDivideConquer/Pangkat.java
@@ -1,25 +1,26 @@
 package P5_BruteForceDivideConquer;
 
 public class Pangkat {
-    public int nilai;
+    public int nilai, pangkat;
 
-    int faktorialBF(int n) {
-        int fakto = 1;
-        int i = 1;
-        while (i<= n) {
-            fakto *= i;
-            i++;
+    int pangkatBF(int a, int n) {
+        int hasil = 1;
+        for (int i = 0; i < n; i++) {
+            hasil *= a;
         }
-        return fakto;
+        return hasil;
     }
 
-    int faktorialDC(int n) {
-        if(n==1){
+    int pangkatDC(int a, int n) {
+        if(n==0){
             return 1;
         } else{
-            int fakto = n * faktorialDC(n-1); // bilangan ganjil
-            return fakto;
-            
+            if (n%2==1) // bilangan ganjil
+            {
+                return (pangkatDC(a,n/2)*pangkatDC(a, n/2)*a);
+            }else{
+                return (pangkatDC(a,n/2)*pangkatDC(a,n/2));
+            }
         }
     }
 }
